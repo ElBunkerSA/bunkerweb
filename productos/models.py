446b394f0ed64e017c23af55a05b58d7d13774e4
AlbertGilMorera <albertgilmorera@gmail.com>
2025-04-07 23:59:27 +0200

    segunda subida

--- productos/models.py
+++ productos/models.py
@@ -3,11 +3,16 @@ from django.db import models
 
 class Producto(models.Model):
     TIPO_CHOICES = [
-        ('raton', 'Ratón'),
-        ('teclado', 'Teclado'),
-        ('mando', 'Mando'),
-        ('casco', 'Casco'),
-        ('monitor', 'Monitor'),
+        ('juegos', 'Juegos'),
+        ('ratones', 'Ratones'),
+        ('teclados', 'Teclados'),
+        ('mandos', 'Mandos'),
+        ('cascos', 'Cascos'),
+        ('otros', 'Otros'),
+    ]
+    SUBTIPO_JUEGOS = [
+        ('fisico', 'Juegos Físicos'),
+        ('digital', 'Juegos Digitales'),
     ]
 
     nombre = models.CharField(max_length=100)
@@ -15,6 +20,12 @@ class Producto(models.Model):
     imagen_url = models.URLField(verbose_name="URL de la imagen")
     afiliado_url = models.URLField(verbose_name="URL de afiliado")
     descripcion = models.TextField(verbose_name="Descripción (por qué recomiendas el producto)")
+    destacado = models.BooleanField(default=False)
+    # Este campo sólo se usará cuando el tipo sea 'juegos'
+    subcategoria = models.CharField(
+        max_length=10, choices=SUBTIPO_JUEGOS, null=True, blank=True,
+        help_text="Usar sólo si el tipo es 'juegos'."
+    )
 
     def __str__(self):
         return self.nombre
--- productos/views.py
+++ productos/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from .models import Producto
 
+
 def index(request):
+    destacados = Producto.objects.filter(destacado=True)
+    juegos_fisicos = Producto.objects.filter(tipo='juegos', subcategoria='fisico')
+    juegos_digitales = Producto.objects.filter(tipo='juegos', subcategoria='digital')
+    ratones = Producto.objects.filter(tipo='ratones')
+    teclados = Producto.objects.filter(tipo='teclados')
+    mandos = Producto.objects.filter(tipo='mandos')
+    cascos = Producto.objects.filter(tipo='cascos')
+    otros = Producto.objects.filter(tipo='otros')
+
     context = {
-        'ratones': Producto.objects.filter(tipo='raton'),
-        'teclados': Producto.objects.filter(tipo='teclado'),
-        'mandos': Producto.objects.filter(tipo='mando'),
-        'monitores': Producto.objects.filter(tipo='monitor'),
-        'cascos': Producto.objects.filter(tipo='casco'),
+        'destacados': destacados,
+        'juegos_fisicos': juegos_fisicos,
+        'juegos_digitales': juegos_digitales,
+        'ratones': ratones,
+        'teclados': teclados,
+        'mandos': mandos,
+        'cascos': cascos,
+        'otros': otros,
     }
     return render(request, 'productos/index.html', context)
