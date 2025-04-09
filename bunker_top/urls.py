c500307bdcf43e28f05a57292442e2a45d28c9ab
AlbertGilMorera <albertgilmorera@gmail.com>
2025-04-09 13:08:34 +0200

    cambios django admin

--- bunker_top/urls.py
+++ bunker_top/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = 'Administración Búnker'
+admin.site.site_title = 'Administración Búnker'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('productos.urls')),
